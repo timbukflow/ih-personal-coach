43d142be65151e148010cd73534144e58aac63bb
Ivo Schwizer <ivoschwizer@gmail.com>
2022-09-14 16:19:40 +0200

    r

---
+++ js/main.js
@@ -0,0 +1,57 @@
+/*loading*/
+
+let i01 = document.querySelector('.i01');
+let h02 = document.querySelector('.h02');
+let h03 = document.querySelector('.h03');
+let loader = document.querySelector('.loader');
+
+window.addEventListener('DOMContentLoaded', ()=>{
+  
+    setTimeout(() => {
+
+        setTimeout(()=>{
+            i01.classList.add('logofade', 'disti01');
+            h02.classList.add('logofade', 'disti02');
+            h03.classList.add('logofade', 'form01');
+        },1000)
+
+        setTimeout(()=>{
+            loader.classList.add('logofade', 'fade');
+        }, 2500)
+
+    }) 
+
+});
+
+/*isinviewport*/
+var isInViewport = function (elem) {
+	var distance = elem.getBoundingClientRect();
+	return (
+		distance.top >= 0 &&
+		distance.left >= 0 &&
+		distance.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+		distance.right <= (window.innerWidth || document.documentElement.clientWidth)
+	);
+};
+
+var findMe = document.querySelector('.find-me');
+var circle = document.querySelectorAll('.circle');
+var circleChild = document.querySelectorAll('.circle h2');
+
+window.addEventListener('scroll', function (event) {
+	if (isInViewport(findMe)) {
+        circle.forEach((circle) => {
+            circle.classList.add('animationC');
+          });
+          circleChild.forEach((circleChild) => {
+            circleChild.classList.add('animationT');
+          });
+	} else {
+        circle.forEach((circle) => {
+            circle.classList.remove('animationC');
+          });
+          circleChild.forEach((circleChild) => {
+            circleChild.classList.remove('animationT');
+          });
+  }
+}, false);
